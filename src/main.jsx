a27a91c817a0c2d17d7f75226dd24074d005a824
FORTRICHE <adourodrigue@gmail.com>
2026-06-11 02:12:41 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-const App = () => <div style={{color:'white',background:'#080C18',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center',fontSize:24}}>🔐 Allô chargement...</div>
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import ALLO from '/src/ALLO.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(<ALLO />)
