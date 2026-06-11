61801c72036d71d74a086bc74e84c5a23cf86e23
FORTRICHE <adourodrigue@gmail.com>
2026-06-11 02:15:32 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ALLO from '/src/ALLO.jsx'
+import ALLO from '../src/ALLO.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(<ALLO />)
